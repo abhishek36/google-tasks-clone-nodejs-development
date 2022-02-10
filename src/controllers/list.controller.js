c197ed96eb9780946d61abd1a33d20e3c81f97cc
abhishek36 <akamble9913@gmail.com>
2022-02-10 11:00:31 +0530

    first commit

---
+++ src/controllers/list.controller.js
@@ -0,0 +1,30 @@
+//import List model here
+
+const addList = (req, res) => {
+  //addList api logic here
+};
+const getListByUserId = (req, res) => {
+  //getListByUserId api logic here
+};
+
+const getListById = (req, res) => {
+  //getListById api logic here
+};
+
+const updateListById = (req, res) => {
+  //updateListById api logic here
+};
+
+const deleteListById = (req, res) => {
+  //deleteListById api logic here
+};
+
+const ListController = {
+  addList,
+  getListByUserId,
+  getListById,
+  updateListById,
+  deleteListById,
+};
+
+module.exports = ListController;
---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,36 @@
+//import Task model here
+
+const addTask = (req, res) => {
+  //addTask api logic here
+};
+
+const getTaskByListId = (req, res) => {
+  //getTaskByListId api logic here
+};
+
+const getTaskByUserId = (req, res) => {
+  //getTaskByUserId api logic here
+};
+
+const getTaskById = (req, res) => {
+  //getTaskById api logic here
+};
+
+const updateTaskById = (req, res) => {
+  //updateTaskById api logic here
+};
+
+const deleteTaskById = (req, res) => {
+  //deleteTaskById api logic here
+};
+
+const TaskController = {
+  addTask,
+  getTaskByListId,
+  getTaskByUserId,
+  getTaskById,
+  updateTaskById,
+  deleteTaskById,
+};
+
+module.exports = TaskController;
